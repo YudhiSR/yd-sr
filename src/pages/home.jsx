4b239a8e60691ca343c7a0326a3b99dd58963368
YudhiSR <yudhisr2023@gmail.com>
2023-08-30 09:28:32 +0700

    cek

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+          <h1 className="text-3xl font-bold underline">HELLO</h1>
+        </div>
+    )
+}
+export default Home
