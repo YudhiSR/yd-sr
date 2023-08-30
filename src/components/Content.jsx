1828a233dd94af587a86dcfa4d49febe8e0a744f
YudhiSR <yudhisr2023@gmail.com>
2023-08-30 13:46:07 +0700

    second

---
+++ src/components/Content.jsx
@@ -0,0 +1,18 @@
+
+
+const Content = () => {
+    return (
+        <div>
+            <img
+  src="https://tecdn.b-cdn.net/img/new/standard/city/041.jpg"
+  class="max-w-sm m-4 mt-6 rounded border bg-white p-1 dark:border-neutral-700 dark:bg-neutral-800"
+  alt="..." />
+            <img
+  src="https://tecdn.b-cdn.net/img/new/standard/city/041.jpg"
+  class="max-w-sm m-4 mt-6 rounded border bg-white p-1 dark:border-neutral-700 dark:bg-neutral-800"
+  alt="..." />
+        </div>
+    )
+}
+
+export default Content
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,11 @@
+import Content from "../components/Content"
 import Navbar from "../components/Navbar"
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
-          <h1 className="text-3xl font-bold underline">HELLO</h1>
+            <Content/>
         </div>
     )
 }
